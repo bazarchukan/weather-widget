d97ad0894b83c4ef983004aff88b84f9affb0966
bazarchukan <bazarchukan@gmail.com>
2023-08-02 15:55:36 +0600

    code refactoring

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { createPinia } from 'pinia'
 import "@/assets/css/app.scss";
 
 const app = createApp(App);
-const pinia = createPinia()
+const pinia = createPinia();
 
 app.use(pinia)
 app.mount("weather-widget");
